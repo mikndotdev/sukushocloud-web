219b85c511dee65f6559ea8b3bb60a2db2cd8ae2
maamokun/MikanDev <maamo-kun@outlook.com>
2024-10-01 22:46:56 +0900

    add compatibility string for Cloudflare Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+  },
   runtimeConfig: {
     logtoClientId: '',
     logtoClientSecret: '',
