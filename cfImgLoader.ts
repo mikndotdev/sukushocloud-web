cfeaf19b0aca9b1571f9911dbe366944be51ad69
maamokun/MikanDev <maamo-kun@outlook.com>
2024-10-20 11:56:54 +0900

    better image loader

--- cfImgLoader.ts
+++ cfImgLoader.ts
@@ -1,12 +1,22 @@
 const normalizeSrc = (src: string) => {
-    return src.startsWith('/') ? src.slice(1) : src;
-  };
-  
-  export default function cloudflareLoader ({ src, width, quality }: { src: string; width: number; quality?: number }) {
+    return src.startsWith("/") ? src.slice(1) : src;
+};
+
+export default function cloudflareLoader({
+    src,
+    width,
+    quality,
+}: { src: string; width: number; quality?: number }) {
+    const isLocal = !src.includes("localhost");
+
+    if (isLocal) {
+        return src;
+    }
+
     const params = [`width=${width}`];
     if (quality) {
-      params.push(`quality=${quality}`);
+        params.push(`quality=${quality}`);
     }
-    const paramsString = params.join(',');
+    const paramsString = params.join(",");
     return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
-  };
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        loader: 'custom',
-        loaderFile: './cfImgLoader.ts',
+        loader: "custom",
+        loaderFile: "./cfImgLoader.ts",
     },
 };
 
