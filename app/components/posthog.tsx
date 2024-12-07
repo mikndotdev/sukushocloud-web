1d9358da4197594682757503d30cf680fe08cdf6
maamokun/MikanDev <maamo-kun@outlook.com>
2024-12-07 23:43:39 +0900

    fix: type error

--- app/components/posthog.tsx
+++ app/components/posthog.tsx
@@ -1,13 +1,14 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import type { ReactNode } from "react";
 
 if (typeof window !== "undefined") {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
         person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     });
 }
-export function CSPostHogProvider({ children }) {
+export function CSPostHogProvider({ children: children }: Readonly<{ children: ReactNode }>) {
     return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
